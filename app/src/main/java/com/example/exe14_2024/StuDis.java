b4b41afaaf81939990ea172694412afb7ab742b7
yd2058 <yd2058@bs.amalnet.k12.il>
2024-04-09 17:40:57 +0300

    Initial commit - end of xmls

---
+++ app/src/main/java/com/example/exe14_2024/StuDis.java
@@ -0,0 +1,19 @@
+package com.example.exe14_2024;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class StuDis extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stu_dis);
+
+    }
+}
---
+++ app/src/main/java/com/example/exe14_2024/StuInEd.java
@@ -0,0 +1,14 @@
+package com.example.exe14_2024;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StuInEd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stuined);
+    }
+}
