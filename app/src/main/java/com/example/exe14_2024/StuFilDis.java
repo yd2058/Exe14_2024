d6b85f220e9ac2dc04229c3410cbf151b8bcc704
yd2058 <yd2058@bs.amalnet.k12.il>
2024-04-09 22:27:09 +0300

    Added context menu

--- app/src/main/java/com/example/exe14_2024/StuFilDis.java
+++ app/src/main/java/com/example/exe14_2024/StuFilDis.java
@@ -1,6 +1,10 @@
 package com.example.exe14_2024;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,12 +12,42 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class StuFilDis extends AppCompatActivity {
+public class StuFilDis extends AppCompatActivity implements View.OnCreateContextMenuListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stu_fil_dis);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    /**
+     * reacts to item selection.
+     * <p>
+     *
+     * @param	item Description	refers to the selected menu item.
+     * @return	Description			returns true.
+     */
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if(id == R.id.cmcr){
+            Intent si = new Intent(this,Credits.class);
+            startActivity(si);
+        }
+        else if (id ==R.id.cmsv) {
+            Intent si = new Intent(this, StuDis.class);
+            startActivity(si);
+        }
+        else if (id ==R.id.cmsi) {
+            Intent si = new Intent(this, StuInEd.class);
+            startActivity(si);
+        }
+        return true;
     }
 }
--- app/src/main/java/com/example/exe14_2024/StuInEd.java
+++ app/src/main/java/com/example/exe14_2024/StuInEd.java
@@ -1,14 +1,48 @@
 package com.example.exe14_2024;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class StuInEd extends AppCompatActivity {
+public class StuInEd extends AppCompatActivity implements View.OnCreateContextMenuListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stuined);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    /**
+     * reacts to item selection.
+     * <p>
+     *
+     * @param	item Description	refers to the selected menu item.
+     * @return	Description			returns true.
+     */
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if(id == R.id.cmcr){
+            Intent si = new Intent(this,Credits.class);
+            startActivity(si);
+        }
+        else if (id ==R.id.cmsv) {
+            Intent si = new Intent(this, StuDis.class);
+            startActivity(si);
+        }
+        else if (id ==R.id.cmsfv) {
+            Intent si = new Intent(this, StuFilDis.class);
+            startActivity(si);
+        }
+        return true;
     }
 }
