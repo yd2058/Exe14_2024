f193dd9e1adb4905dee6f6b80e95a52a6e631021
yd2058 <yd2058@bs.amalnet.k12.il>
2024-04-20 15:31:46 +0300

    corrected input check and added Firebase helper

--- app/src/main/java/com/example/exe14_2024/StuInEd.java
+++ app/src/main/java/com/example/exe14_2024/StuInEd.java
@@ -17,6 +17,10 @@ import android.widget.Toast;
 import android.widget.ToggleButton;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.exe14_2024.Helpers.Student;
+import com.example.exe14_2024.Helpers.Vaccine;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class StuInEd extends AppCompatActivity implements View.OnCreateContextMenuListener{
     TextView tvid, tvfn, tvln, tvv1, tvv2, tvcls;
     EditText stid, stfn, stln, clsnum, v1d, v1m, v1y, v1l, v2d, v2m, v2y, v2l;
@@ -26,6 +30,8 @@ public class StuInEd extends AppCompatActivity implements View.OnCreateContextMe
     Boolean takin, edit = false;
     int grdslc=0;
     String day1, day2, month1, month2, year1, year2;
+    Student tempst;
+    Vaccine tmpvac1, tmpvac2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -105,29 +111,79 @@ public class StuInEd extends AppCompatActivity implements View.OnCreateContextMe
         else{tvcls.setTextColor(0xff1B1B1F);}
         if(grdslc<7||grdslc>12){takin = false;tvcls.setTextColor(0xffff0000);}//checks if there is a valid grade
         else{tvcls.setTextColor(0xff1B1B1F);}
-        if((!day1.matches("\\d+"))||day1.isEmpty()||Integer.parseInt(day1)<1||Integer.parseInt(day1)>31){takin = false;tvv1.setTextColor(0xffff0000);}//checks if vaccine 1 is on a valid day
-        else{tvv1.setTextColor(0xff1B1B1F);}
-        if((!month1.matches("\\d+"))||month1.isEmpty()||Integer.parseInt(month1)<1||Integer.parseInt(month1)>12) {takin = false;tvv1.setTextColor(0xffff0000);}//checks if vaccine 1 is on a valid month
-        else{tvv1.setTextColor(0xff1B1B1F);}
-        if((!year1.matches("\\d+"))||year1.isEmpty()){takin = false;tvv1.setTextColor(0xffff0000);}//checks if vaccine 1 is on a valid year
-        else{tvv1.setTextColor(0xff1B1B1F);}
-        if(v1l.getText().toString().isEmpty()){takin = false;tvv1.setTextColor(0xffff0000);}//checks if vaccine 1 has a location
-        else{tvv1.setTextColor(0xff1B1B1F);}
-        if((!day2.matches("\\d+"))||day2.isEmpty()||Integer.parseInt(day2)<1||Integer.parseInt(day2)>31||isodmytoymd(Integer.parseInt(day2),Integer.parseInt(month2),Integer.parseInt(year2))<isodmytoymd(Integer.parseInt(day1),Integer.parseInt(month1),Integer.parseInt(year1))) {takin = false;tvv2.setTextColor(0xffff0000);}//checks if vaccine 1 is on a valid day
-        else{tvv2.setTextColor(0xff1B1B1F);}
-        if((!month2.matches("\\d+"))||month2.isEmpty()||Integer.parseInt(month2)<1||Integer.parseInt(month2)>12) {takin = false;tvv2.setTextColor(0xffff0000);}//checks if vaccine 1 is on a valid month
-        else{tvv2.setTextColor(0xff1B1B1F);}
-        if((!year2.matches("\\d+"))||year2.isEmpty()){takin = false;tvv2.setTextColor(0xffff0000);}//checks if vaccine 1 is on a valid year
-        else{tvv2.setTextColor(0xff1B1B1F);}
-        if(v2l.getText().toString().isEmpty()){takin = false;tvv2.setTextColor(0xffff0000);}//checks if vaccine 1 has a location
-        else{tvv2.setTextColor(0xff1B1B1F);}
+        if(vacbtn.isChecked()) {
+            if ((!day1.matches("\\d+")) || day1.isEmpty() || Integer.parseInt(day1) < 1 || Integer.parseInt(day1) > 31) {
+                takin = false;
+                v1d.setHighlightColor((0xffff0000));
+            }//checks if vaccine 1 is on a valid day
+            else {
+                v1d.setHighlightColor(0xff1B1B1F);
+            }
+            if ((!month1.matches("\\d+")) || month1.isEmpty() || Integer.parseInt(month1) < 1 || Integer.parseInt(month1) > 12) {
+                takin = false;
+                v1m.setHighlightColor(0xffff0000);
+            }//checks if vaccine 1 is on a valid month
+            else {
+                v1m.setHighlightColor(0xff1B1B1F);
+            }
+            if ((!year1.matches("\\d+")) || year1.isEmpty()) {
+                takin = false;
+                v1y.setHighlightColor(0xffff0000);
+            }//checks if vaccine 1 is on a valid year
+            else {
+                v1y.setHighlightColor(0xff1B1B1F);
+            }
+            if (v1l.getText().toString().isEmpty()) {
+                takin = false;
+                v1l.setHighlightColor(0xffff0000);
+            }//checks if vaccine 1 has a location
+            else {
+                v1l.setHighlightColor(0xff1B1B1F);
+            }
+            if ((!day2.matches("\\d+")) || day2.isEmpty() || Integer.parseInt(day2) < 1 || Integer.parseInt(day2) > 31) {
+                takin = false;
+                v2d.setHighlightColor(0xffff0000);
+            }//checks if vaccine 2 is on a valid day
+            else {
+                v2d.setHighlightColor(0xff1B1B1F);
+            }
+            if ((!month2.matches("\\d+")) || month2.isEmpty() || Integer.parseInt(month2) < 1 || Integer.parseInt(month2) > 12) {
+                takin = false;
+                v2m.setHighlightColor(0xffff0000);
+            }//checks if vaccine 2 is on a valid month
+            else {
+                v2m.setHighlightColor(0xff1B1B1F);
+            }
+            if ((!year2.matches("\\d+")) || year2.isEmpty()) {
+                takin = false;
+                v2y.setHighlightColor(0xffff0000);
+            }//checks if vaccine 2 is on a valid year
+            else {
+                v2y.setHighlightColor(0xff1B1B1F);
+            }
+            if (v2l.getText().toString().isEmpty()) {
+                takin = false;
+                v2l.setHighlightColor(0xffff0000);
+            }//checks if vaccine 2 has a location
+            else {
+                v2l.setHighlightColor(0xff1B1B1F);
+            }
+            if (isodmytoymd(Integer.parseInt(day2), Integer.parseInt(month2), Integer.parseInt(year2)) < isodmytoymd(Integer.parseInt(day1), Integer.parseInt(month1), Integer.parseInt(year1))) {
+                takin = false;
+                tvv2.setHighlightColor(0xffff0000);
+                Toast.makeText(this, "Invalid  spacing since vaccine 1", Toast.LENGTH_SHORT).show();
+            }//checks if vaccine 2 is on valid spacing from vaccine 1
+            else {
+                tvv2.setHighlightColor(0xff1B1B1F);
+            }
+        }
 
         if(takin){
             if(edit){}//edit previous
             else{}//save new
         }
         else{
-            Toast.makeText(this, "there was an issue with the reddened values", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "reddened values invalid", Toast.LENGTH_SHORT).show();
         }
     }
     
